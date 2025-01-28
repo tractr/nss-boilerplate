efe27eaddcfef40f83656c3045db8b49c2017aba
GlennTractr <75043086+GlennTractr@users.noreply.github.com>
2025-01-28 11:34:32 -0600

    refacto: snake case (#3)

---
+++ sqltosave.sql
@@ -0,0 +1,114 @@
+-- Example: enable the "pg_net" extension.
+create extension pg_net;
+-- Note: The extension creates its own schema/namespace named "net" to avoid naming conflicts.
+
+-- Example: disable the "pg_net" extension
+drop extension if exists pg_net;
+drop schema net;
+
+
+CREATE OR REPLACE FUNCTION create_stream_ai_menu(menu uuid)
+RETURNS TABLE(run_id uuid, context_id uuid) AS $$
+DECLARE
+    new_run_id uuid;
+BEGIN
+    -- Create a new run and get the generated UUID
+    new_run_id := create_stream_ai_run('menu');
+
+    -- Insert into stream_ai_menu_run_context and get the context ID
+    INSERT INTO public."stream_ai_menu_run_context" (menu, run)
+    VALUES (menu, new_run_id)
+    RETURNING id INTO context_id;
+
+    -- Return both the run ID and the context ID
+    RETURN QUERY SELECT new_run_id, context_id;
+END;
+$$ LANGUAGE plpgsql SECURITY INVOKER;
+
+
+
+
+
+CREATE OR REPLACE FUNCTION create_stream_ai_run(type public."stream_ai_type")
+RETURNS uuid AS $$
+DECLARE
+    new_id uuid;
+BEGIN
+    INSERT INTO public."StreamAIRun" (type)
+    VALUES (type)
+    RETURNING id INTO new_id;
+
+    RETURN new_id;
+END;
+$$ LANGUAGE plpgsql SECURITY INVOKER;
+
+
+
+
+-- Create the trigger function
+CREATE OR REPLACE FUNCTION trigger_create_stream_ai_menu()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Check if the operation is an insert or update
+    IF (TG_OP = 'INSERT' AND NEW.file IS NOT NULL) OR 
+       (TG_OP = 'UPDATE' AND NEW.file IS NOT NULL AND NEW.file <> OLD.file) THEN
+        -- Call the create_stream_ai_menu function with the menu UUID
+        PERFORM create_stream_ai_menu(NEW.id);
+    END IF;
+
+    RETURN NEW; -- Return the new record
+END;
+$$ LANGUAGE plpgsql SECURITY INVOKER;
+
+-- Create the trigger on the Menu table
+CREATE TRIGGER create_stream_ai_menu_trigger
+AFTER INSERT OR UPDATE ON public."Menu"
+FOR EACH ROW
+EXECUTE FUNCTION trigger_create_stream_ai_menu();
+
+
+
+
+
+
+-- Create the trigger function
+CREATE OR REPLACE FUNCTION trigger_post_api_call()
+RETURNS TRIGGER AS $$
+DECLARE
+    api_url text;
+BEGIN
+    -- Construct the API URL with the specified UUID from the run field
+    api_url := 'http://host.docker.internal:8000/stream-ai/' || NEW.run || '/process';
+
+    -- Make the POST request to the API
+    PERFORM net.http_post(
+        url:=api_url,
+        body:='{}'::jsonb
+    );
+
+    RETURN NEW; -- Return the new record
+END;
+$$ LANGUAGE plpgsql SECURITY INVOKER;
+
+-- Create the trigger on the stream_ai_menu_run_context table
+CREATE TRIGGER post_api_call_trigger
+AFTER INSERT ON public."stream_ai_menu_run_context"
+FOR EACH ROW
+EXECUTE FUNCTION trigger_post_api_call();
+
+
+
+
+
+CREATE OR REPLACE FUNCTION get_run_owner(run uuid)
+RETURNS uuid AS $$
+DECLARE
+    run_owner uuid;
+BEGIN
+    SELECT owner FROM public."stream_ai_run" WHERE id = run INTO run_owner;
+
+    RETURN run_owner;
+END;
+$$ LANGUAGE plpgsql SECURITY INVOKER;
+
+SELECT * FROM get_run_owner('b2147c5b-777b-4c87-bbb1-eda92a98b8ed');
--- supabase/migrations/20250122184257_init_schema.sql
+++ supabase/migrations/20250122184257_init_schema.sql
@@ -72,19 +72,19 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
 
 
-CREATE TYPE "public"."StreamAIProcessStatus" AS ENUM (
+CREATE TYPE "public"."stream_ai_process_status" AS ENUM (
     'created',
     'processing',
-    'step-finished',
-    'fully-finished',
+    'step_finished',
+    'fully_finished',
     'error'
 );
 
 
-ALTER TYPE "public"."StreamAIProcessStatus" OWNER TO "postgres";
+ALTER TYPE "public"."stream_ai_process_status" OWNER TO "postgres";
 
 
-CREATE TYPE "public"."StreamAIStep" AS ENUM (
+CREATE TYPE "public"."stream_ai_step" AS ENUM (
     'menu_ocr',
     'menu_recipe',
     'menu_environmental_impact',
@@ -92,32 +92,32 @@ CREATE TYPE "public"."StreamAIStep" AS ENUM (
 );
 
 
-ALTER TYPE "public"."StreamAIStep" OWNER TO "postgres";
+ALTER TYPE "public"."stream_ai_step" OWNER TO "postgres";
 
 
-CREATE TYPE "public"."StreamAIType" AS ENUM (
+CREATE TYPE "public"."stream_ai_type" AS ENUM (
     'menu',
     'sell'
 );
 
 
-ALTER TYPE "public"."StreamAIType" OWNER TO "postgres";
+ALTER TYPE "public"."stream_ai_type" OWNER TO "postgres";
 
 SET default_tablespace = '';
 
 SET default_table_access_method = "heap";
 
 
-CREATE TABLE IF NOT EXISTS "public"."Config" (
+CREATE TABLE IF NOT EXISTS "public"."config" (
     "key" "text" NOT NULL,
     "value" "text" NOT NULL
 );
 
 
-ALTER TABLE "public"."Config" OWNER TO "postgres";
+ALTER TABLE "public"."config" OWNER TO "postgres";
 
 
-CREATE TABLE IF NOT EXISTS "public"."File" (
+CREATE TABLE IF NOT EXISTS "public"."file" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "file_bucket" "text" NOT NULL,
@@ -125,10 +125,10 @@ CREATE TABLE IF NOT EXISTS "public"."File" (
 );
 
 
-ALTER TABLE "public"."File" OWNER TO "postgres";
+ALTER TABLE "public"."file" OWNER TO "postgres";
 
 
-CREATE TABLE IF NOT EXISTS "public"."Menu" (
+CREATE TABLE IF NOT EXISTS "public"."menu" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "updated_date" timestamp with time zone,
@@ -139,10 +139,10 @@ CREATE TABLE IF NOT EXISTS "public"."Menu" (
 );
 
 
-ALTER TABLE "public"."Menu" OWNER TO "postgres";
+ALTER TABLE "public"."menu" OWNER TO "postgres";
 
 
-CREATE TABLE IF NOT EXISTS "public"."Sell" (
+CREATE TABLE IF NOT EXISTS "public"."sell" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "updated_date" timestamp with time zone,
@@ -153,14 +153,14 @@ CREATE TABLE IF NOT EXISTS "public"."Sell" (
 );
 
 
-ALTER TABLE "public"."Sell" OWNER TO "postgres";
+ALTER TABLE "public"."sell" OWNER TO "postgres";
 
 
-COMMENT ON TABLE "public"."Sell" IS 'This is a duplicate of Menu';
+COMMENT ON TABLE "public"."sell" IS 'This is a duplicate of menu';
 
 
 
-CREATE TABLE IF NOT EXISTS "public"."StreamAIMenuRunContext" (
+CREATE TABLE IF NOT EXISTS "public"."stream_ai_menu_run_context" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "updated_at" timestamp with time zone,
@@ -169,39 +169,39 @@ CREATE TABLE IF NOT EXISTS "public"."StreamAIMenuRunContext" (
 );
 
 
-ALTER TABLE "public"."StreamAIMenuRunContext" OWNER TO "postgres";
+ALTER TABLE "public"."stream_ai_menu_run_context" OWNER TO "postgres";
 
 
-CREATE TABLE IF NOT EXISTS "public"."StreamAIRun" (
+CREATE TABLE IF NOT EXISTS "public"."stream_ai_run" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "updated_at" timestamp with time zone,
-    "status" "public"."StreamAIProcessStatus" DEFAULT 'created'::"public"."StreamAIProcessStatus" NOT NULL,
+    "status" "public"."stream_ai_process_status" DEFAULT 'created'::"public"."stream_ai_process_status" NOT NULL,
     "error_message" "text",
-    "current_step" "public"."StreamAIStep",
-    "type" "public"."StreamAIType" NOT NULL
+    "current_step" "public"."stream_ai_step",
+    "type" "public"."stream_ai_type" NOT NULL
 );
 
 
-ALTER TABLE "public"."StreamAIRun" OWNER TO "postgres";
+ALTER TABLE "public"."stream_ai_run" OWNER TO "postgres";
 
 
-CREATE TABLE IF NOT EXISTS "public"."StreamAIRunStep" (
+CREATE TABLE IF NOT EXISTS "public"."stream_ai_run_step" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "run" "uuid" NOT NULL,
     "input" "jsonb" NOT NULL,
     "output" "jsonb",
     "finished_at" timestamp with time zone,
-    "step" "public"."StreamAIStep" NOT NULL,
+    "step" "public"."stream_ai_step" NOT NULL,
     "error_message" "text"
 );
 
 
-ALTER TABLE "public"."StreamAIRunStep" OWNER TO "postgres";
+ALTER TABLE "public"."stream_ai_run_step" OWNER TO "postgres";
 
 
-CREATE TABLE IF NOT EXISTS "public"."StreamAISellRunContext" (
+CREATE TABLE IF NOT EXISTS "public"."stream_ai_sell_run_context" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "updated_at" timestamp with time zone,
@@ -210,121 +210,121 @@ CREATE TABLE IF NOT EXISTS "public"."StreamAISellRunContext" (
 );
 
 
-ALTER TABLE "public"."StreamAISellRunContext" OWNER TO "postgres";
+ALTER TABLE "public"."stream_ai_sell_run_context" OWNER TO "postgres";
 
 
-ALTER TABLE ONLY "public"."Config"
-    ADD CONSTRAINT "Config_pkey" PRIMARY KEY ("key");
+ALTER TABLE ONLY "public"."config"
+    ADD CONSTRAINT "config_pkey" PRIMARY KEY ("key");
 
 
 
-ALTER TABLE ONLY "public"."File"
-    ADD CONSTRAINT "File_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."file"
+    ADD CONSTRAINT "file_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."StreamAIMenuRunContext"
-    ADD CONSTRAINT "MenuProcessState_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."stream_ai_menu_run_context"
+    ADD CONSTRAINT "stream_ai_menu_run_context_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."Menu"
-    ADD CONSTRAINT "Menu_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."menu"
+    ADD CONSTRAINT "menu_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."Sell"
-    ADD CONSTRAINT "Sell_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."sell"
+    ADD CONSTRAINT "sell_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."StreamAIRunStep"
-    ADD CONSTRAINT "StreamAIHistory_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."stream_ai_run_step"
+    ADD CONSTRAINT "stream_ai_run_step_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."StreamAIRun"
-    ADD CONSTRAINT "StreamAIProcessState_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."stream_ai_run"
+    ADD CONSTRAINT "stream_ai_run_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."StreamAISellRunContext"
-    ADD CONSTRAINT "StreamAISellState_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY "public"."stream_ai_sell_run_context"
+    ADD CONSTRAINT "stream_ai_sell_run_context_pkey" PRIMARY KEY ("id");
 
 
 
-ALTER TABLE ONLY "public"."File"
-    ADD CONSTRAINT "File_file_bucket_file_path_fkey" FOREIGN KEY ("file_bucket", "file_path") REFERENCES "storage"."objects"("bucket_id", "name") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."file"
+    ADD CONSTRAINT "file_file_bucket_file_path_fkey" FOREIGN KEY ("file_bucket", "file_path") REFERENCES "storage"."objects"("bucket_id", "name") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."StreamAIMenuRunContext"
-    ADD CONSTRAINT "MenuProcessState_menu_fkey" FOREIGN KEY ("menu") REFERENCES "public"."Menu"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."stream_ai_menu_run_context"
+    ADD CONSTRAINT "stream_ai_menu_run_context_menu_fkey" FOREIGN KEY ("menu") REFERENCES "public"."menu"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."Menu"
-    ADD CONSTRAINT "Menu_file_fkey" FOREIGN KEY ("file") REFERENCES "public"."File"("id") ON DELETE SET NULL;
+ALTER TABLE ONLY "public"."menu"
+    ADD CONSTRAINT "menu_file_fkey" FOREIGN KEY ("file") REFERENCES "public"."file"("id") ON DELETE SET NULL;
 
 
 
-ALTER TABLE ONLY "public"."Menu"
-    ADD CONSTRAINT "Menu_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."menu"
+    ADD CONSTRAINT "menu_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."Sell"
-    ADD CONSTRAINT "Sell_file_fkey" FOREIGN KEY ("file") REFERENCES "public"."File"("id") ON DELETE SET NULL;
+ALTER TABLE ONLY "public"."sell"
+    ADD CONSTRAINT "sell_file_fkey" FOREIGN KEY ("file") REFERENCES "public"."file"("id") ON DELETE SET NULL;
 
 
 
-ALTER TABLE ONLY "public"."Sell"
-    ADD CONSTRAINT "Sell_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."sell"
+    ADD CONSTRAINT "sell_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."StreamAIRunStep"
-    ADD CONSTRAINT "StreamAIHistory_state_fkey" FOREIGN KEY ("run") REFERENCES "public"."StreamAIRun"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."stream_ai_run_step"
+    ADD CONSTRAINT "stream_ai_run_step_run_fkey" FOREIGN KEY ("run") REFERENCES "public"."stream_ai_run"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."StreamAIMenuRunContext"
-    ADD CONSTRAINT "StreamAIMenuState_state_fkey" FOREIGN KEY ("run") REFERENCES "public"."StreamAIRun"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."stream_ai_menu_run_context"
+    ADD CONSTRAINT "stream_ai_menu_run_context_run_fkey" FOREIGN KEY ("run") REFERENCES "public"."stream_ai_run"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."StreamAISellRunContext"
-    ADD CONSTRAINT "StreamAISellState_sell_fkey" FOREIGN KEY ("sell") REFERENCES "public"."Sell"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."stream_ai_sell_run_context"
+    ADD CONSTRAINT "stream_ai_sell_run_context_sell_fkey" FOREIGN KEY ("sell") REFERENCES "public"."sell"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE ONLY "public"."StreamAISellRunContext"
-    ADD CONSTRAINT "StreamAISellState_state_fkey" FOREIGN KEY ("run") REFERENCES "public"."StreamAIRun"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."stream_ai_sell_run_context"
+    ADD CONSTRAINT "stream_ai_sell_run_context_run_fkey" FOREIGN KEY ("run") REFERENCES "public"."stream_ai_run"("id") ON DELETE CASCADE;
 
 
 
-ALTER TABLE "public"."Config" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."config" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."File" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."file" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."Menu" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."menu" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."Sell" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."sell" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."StreamAIMenuRunContext" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."stream_ai_menu_run_context" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."StreamAIRun" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."stream_ai_run" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."StreamAIRunStep" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."stream_ai_run_step" ENABLE ROW LEVEL SECURITY;
 
 
-ALTER TABLE "public"."StreamAISellRunContext" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."stream_ai_sell_run_context" ENABLE ROW LEVEL SECURITY;
 
 
 
@@ -540,51 +540,51 @@ GRANT USAGE ON SCHEMA "public" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."Config" TO "anon";
-GRANT ALL ON TABLE "public"."Config" TO "authenticated";
-GRANT ALL ON TABLE "public"."Config" TO "service_role";
+GRANT ALL ON TABLE "public"."config" TO "anon";
+GRANT ALL ON TABLE "public"."config" TO "authenticated";
+GRANT ALL ON TABLE "public"."config" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."File" TO "anon";
-GRANT ALL ON TABLE "public"."File" TO "authenticated";
-GRANT ALL ON TABLE "public"."File" TO "service_role";
+GRANT ALL ON TABLE "public"."file" TO "anon";
+GRANT ALL ON TABLE "public"."file" TO "authenticated";
+GRANT ALL ON TABLE "public"."file" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."Menu" TO "anon";
-GRANT ALL ON TABLE "public"."Menu" TO "authenticated";
-GRANT ALL ON TABLE "public"."Menu" TO "service_role";
+GRANT ALL ON TABLE "public"."menu" TO "anon";
+GRANT ALL ON TABLE "public"."menu" TO "authenticated";
+GRANT ALL ON TABLE "public"."menu" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."Sell" TO "anon";
-GRANT ALL ON TABLE "public"."Sell" TO "authenticated";
-GRANT ALL ON TABLE "public"."Sell" TO "service_role";
+GRANT ALL ON TABLE "public"."sell" TO "anon";
+GRANT ALL ON TABLE "public"."sell" TO "authenticated";
+GRANT ALL ON TABLE "public"."sell" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."StreamAIMenuRunContext" TO "anon";
-GRANT ALL ON TABLE "public"."StreamAIMenuRunContext" TO "authenticated";
-GRANT ALL ON TABLE "public"."StreamAIMenuRunContext" TO "service_role";
+GRANT ALL ON TABLE "public"."stream_ai_menu_run_context" TO "anon";
+GRANT ALL ON TABLE "public"."stream_ai_menu_run_context" TO "authenticated";
+GRANT ALL ON TABLE "public"."stream_ai_menu_run_context" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."StreamAIRun" TO "anon";
-GRANT ALL ON TABLE "public"."StreamAIRun" TO "authenticated";
-GRANT ALL ON TABLE "public"."StreamAIRun" TO "service_role";
+GRANT ALL ON TABLE "public"."stream_ai_run" TO "anon";
+GRANT ALL ON TABLE "public"."stream_ai_run" TO "authenticated";
+GRANT ALL ON TABLE "public"."stream_ai_run" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."StreamAIRunStep" TO "anon";
-GRANT ALL ON TABLE "public"."StreamAIRunStep" TO "authenticated";
-GRANT ALL ON TABLE "public"."StreamAIRunStep" TO "service_role";
+GRANT ALL ON TABLE "public"."stream_ai_run_step" TO "anon";
+GRANT ALL ON TABLE "public"."stream_ai_run_step" TO "authenticated";
+GRANT ALL ON TABLE "public"."stream_ai_run_step" TO "service_role";
 
 
 
-GRANT ALL ON TABLE "public"."StreamAISellRunContext" TO "anon";
-GRANT ALL ON TABLE "public"."StreamAISellRunContext" TO "authenticated";
-GRANT ALL ON TABLE "public"."StreamAISellRunContext" TO "service_role";
+GRANT ALL ON TABLE "public"."stream_ai_sell_run_context" TO "anon";
+GRANT ALL ON TABLE "public"."stream_ai_sell_run_context" TO "authenticated";
+GRANT ALL ON TABLE "public"."stream_ai_sell_run_context" TO "service_role";
 
 
 
